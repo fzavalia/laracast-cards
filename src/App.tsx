4272427af4746c5510a74a42b3286983b0177380
Fernando Zavalia <fzavalia@mulesoft.com>
2019-11-22 00:55:41 -0300

    Add svg icons + css module

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from "react";
-import "./App.css";
+import styles from "./App.module.css";
+import { ReactComponent as LessonsIcon } from "./lessons.svg";
+import { ReactComponent as TimeIcon } from "./time.svg";
 
 const App: React.FC = () => {
   return (
-    <div className="cards">
+    <div className={styles.cards}>
       <Card />
       <Card />
       <Card />
@@ -15,42 +17,42 @@ const App: React.FC = () => {
 
 const Card = () => {
   return (
-    <div className="card">
-      <div className="left">
-        <div className="technology">Laravel</div>
-        <div className="logo">
+    <div className={styles.card}>
+      <div className={styles.left}>
+        <div className={styles.technology}>Laravel</div>
+        <div className={styles.logo}>
           <img
             src="https://laracasts.com/images/series/2018/pngs/laravel-6-from-scratch.png"
             alt={"..."}
           />
         </div>
-        <div className="difficulty">
-          <div className="difficulty-graph">
-            <div className="difficulty-graph-bar off" />
-            <div className="difficulty-graph-bar" />
-            <div className="difficulty-graph-bar" />
+        <div className={styles.difficulty}>
+          <div className={styles.difficultyGraph}>
+            <div className={styles.difficultyGraphBar} />
+            <div className={styles.difficultyGraphBar} />
+            <div className={styles.difficultyGraphBar} />
           </div>
-          <div className="difficulty-detail">
-            <div className="difficulty-detail-difficulty">Intermediate</div>
-            <div className="difficulty-detail-text">Difficulty</div>
+          <div className={styles.difficultyDetail}>
+            <div>Intermediate</div>
+            <div>Difficulty</div>
           </div>
         </div>
       </div>
-      <div className="right">
-        <div className="title">Laravel 6 From Scratch</div>
-        <div className="description">
+      <div className={styles.right}>
+        <div className={styles.title}>Laravel 6 From Scratch</div>
+        <div className={styles.description}>
           In this series, step by step, I'll show you how to build web
           applications with Laravel 6. We'll start with the basics and
           incrementally dig deeper and...
         </div>
-        <div className="bottom">
-          <div className="lessons">
-            <div className="lessons-icon" />
-            <div className="lessons-text">36 Lessons</div>
+        <div className={styles.bottom}>
+          <div className={styles.bottomElement}>
+            <LessonsIcon />
+            <div className={styles.bottomText}>36 Lessons</div>
           </div>
-          <div className="duration">
-            <div className="duration-icon" />
-            <div className="duration-text">3:34:06 hrs</div>
+          <div className={styles.bottomElement}>
+            <TimeIcon />
+            <div className={styles.bottomText}>3:34:06 hrs</div>
           </div>
         </div>
       </div>
